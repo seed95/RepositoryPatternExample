55ad4f2d10383c329d5ec4ffdaaeef94fc2317b6
sajad <sd.ghadir@gmail.com>
2019-08-24 11:59:24 +0430

    Main application

---
+++ DataBase.java
@@ -0,0 +1,11 @@
+
+// This class is for save and retrieve data from DataBase
+public class DataBase {
+
+
+
+	public String getUser() {
+		return "name: akbar , lastname: hashemi";
+	}
+
+}
---
+++ Server.java
@@ -0,0 +1,8 @@
+
+// This class is for save and retrieve data from Server
+public class Server {
+
+	public byte[] getUser() {
+		return  "name: sajad , lastname: sajadi".getBytes();
+	}
+}
